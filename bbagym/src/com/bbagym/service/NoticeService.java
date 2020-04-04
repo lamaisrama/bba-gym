28d88e0eb0b1ce45680808acb23f2276ba657887
Serenagayoungchoi <Serenagayoungchoi@192.168.35.227>
2020-04-05 00:48:29 +0900

    notice... 추가 / noticelist.jsp 수정  - choi
    noticedao
    noticeservice
    noticelistservlet
    notice-query.properties

---
+++ bbagym/src/com/bbagym/service/NoticeService.java
@@ -0,0 +1,34 @@
+package com.bbagym.service;
+
+import java.sql.Connection;
+import java.util.List;
+
+import static com.bbagym.common.JDBCTemplate.getConnection;
+import static com.bbagym.common.JDBCTemplate.close;
+import static com.bbagym.common.JDBCTemplate.commit;
+import static com.bbagym.common.JDBCTemplate.rollback;
+
+import com.bbagym.dao.NoticeDao;
+import com.bbagym.model.vo.Notice;
+
+public class NoticeService {
+
+	private NoticeDao dao = new NoticeDao();
+
+	public List<Notice> selectNoticeList(int cPage, int numPerPage) {
+		Connection conn = getConnection();
+		List<Notice> list = dao.selectNoticeList(conn, cPage, numPerPage);
+		close(conn);
+		return list;
+	}
+
+	public int selectNoticeCount() {
+		Connection conn = getConnection();
+		int count = dao.selectNoticeCount(conn);
+		close(conn);
+		return count;
+	}
+	
+	
+	
+}
